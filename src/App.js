38c7d4ccc1586710db2fbd48b3c442198e911668
OfirNassimi <ofir3nassimi@gmail.com>
2023-04-30 15:44:27 +0300

    elevator system

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import './App.css';
+import ElevatorSystem from './components/ElevatorSystem/ElevatorSystem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className='app'>
+        <b className='title'>Elevator Exercise</b>
+        <div className='system-container'>
+          <ElevatorSystem numOfElevators={5} numOfFloors={10} />
+        </div>
+      </div>
+    </Provider>
   );
 }
 
